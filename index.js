f8a643574b62883234258bc6ca85f02ef4dc2007
communal-documentation-0347 <scottkoons@gmail.com>
2020-07-21 03:08:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,25 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase();
 }
+
+function logShout(string) {
+  return string.toUpperCase();
+}
+console.log(logShout('hello'));
+
+function logWhisper(string) {
+  return string.toLowerCase();
+}
+console.log(logWhisper("HELLO"));
+
+function sayHiToGandma(string) {
+    if (string == string.toUpperCase()) {
+        return "YES INDEED!";
+    }
+    else if (string == string.toLowerCase()) {
+        return "I can't hear you!";
+    } else if (string == "I love you, Grandma.") {
+        return "I love you, too.";
+    }
+}
+console.log(sayHiToGandma('I love you, Grandma!'));
